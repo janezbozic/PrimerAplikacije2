0490920fa2c26a55ed4a2b3f34b6d8cb263d4d47
janezbozic <janez.bozic1@gmail.com>
2020-03-30 15:01:58 +0200

    Removed permissions we don't need

--- app/src/main/java/si/uni_lj/fri/pbd/miniapp1/ui/messages/MessageFragment.java
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp1/ui/messages/MessageFragment.java
@@ -1,24 +1,17 @@
 package si.uni_lj.fri.pbd.miniapp1.ui.messages;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 
 import java.util.LinkedList;
 
@@ -28,11 +21,7 @@ import si.uni_lj.fri.pbd.miniapp1.ui.contacts.ContactsViewModel;
 
 public class MessageFragment extends Fragment {
 
-    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 34;
-    private static final int MY_PERMISSIONS_REQUEST_SEND_EMAIL = 35;
     private ContactsViewModel contactsViewModel;
-
-    // PROBI DODAT PERMISSIONE
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -47,7 +36,7 @@ public class MessageFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 if (contactsViewModel.getCheckedContacts() != null && contactsViewModel.getCheckedContacts().size() > 0)
-                    permissionEmail();
+                    sendEmail(contactsViewModel.getCheckedContacts());
                 else
                     Toast.makeText(getActivity(), "The are no contacts selected. Please select one or more.",Toast.LENGTH_SHORT).show();
             }
@@ -57,53 +46,13 @@ public class MessageFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 if (contactsViewModel.getCheckedContacts() != null && contactsViewModel.getCheckedContacts().size() > 0)
-                    permissionMMS();
+                    sendMMS(contactsViewModel.getCheckedContacts());
                 else
                     Toast.makeText(getActivity(), "The are no contacts selected. Please select one or more.",Toast.LENGTH_SHORT).show();
             }
         });
 
         return root;
-    }
-
-    public void permissionMMS(){
-        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST_SEND_SMS);
-        }
-        else {
-            sendMMS(contactsViewModel.getCheckedContacts());
-        }
-    }
-
-    public void permissionEmail(){
-        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.INTERNET}, MY_PERMISSIONS_REQUEST_SEND_EMAIL);
-        }
-        else {
-            sendEmail(contactsViewModel.getCheckedContacts());
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    sendMMS(contactsViewModel.getCheckedContacts());
-                } else {
-                    Toast.makeText(getActivity(), "Permission for sending MMS messages not granted.",Toast.LENGTH_SHORT).show();
-                }
-                return;
-            }
-            case MY_PERMISSIONS_REQUEST_SEND_EMAIL: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    sendEmail(contactsViewModel.getCheckedContacts());
-                } else {
-                    Toast.makeText(getActivity(), "Permission for accessing to internet not granted.",Toast.LENGTH_SHORT).show();
-                }
-                return;
-            }
-        }
     }
 
     private void sendEmail(LinkedList<Contact> checkedContacts){
